d0ff479baa6471a7f39039e007cb2157499cb21f
danjos06 <dananjos97@gmail.com>
2024-10-13 15:03:39 -0300

    navbar2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function Menu(e){
     let list = document.querySelector('ul');
-
-    e.class === 'menu' ? (e.class = "close" ,list.classList.add('top-[-80px]'),list.classList.add('opacity-100')) : (e.class = "menu" ,list.classList.remove('top-[-80px]'),list.classList.remove('opacity-100'))
+    
+    e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
 }
